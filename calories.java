273ee6c46d18228ccb5d474cf6a2980c7c988442
Naorvi <55125540+Naorvi@users.noreply.github.com>
2019-09-21 14:36:03 -0400

    Update calories.java

--- calories.java
+++ calories.java
@@ -1,56 +1,74 @@
 package calories;
 
-import javax.swing.JOptionPane; //needed to use the dialog box
+import javax.swing.JOptionPane; // needed to use the dialog box
+
 public class calories {
 
-	public static void main(String[] args) {
-		//Declare variables
-				double weight = 0.0;
-				double totalCalories = 0.0;
-				String userName; //needed to store input from a dialog box
-				String userWeight; //needed to store input from a dialog box
-				String stringInput; //needed to store input from a dialog box
-				String gender;//needed to store input from a dialog box
-				boolean male;						
-				String exercise;//needed to store input from a dialog box
-				boolean moderate = false;
-				boolean low = false;
-				
-				
-				//Display intro using a dialog box
-				JOptionPane.showMessageDialog(null, "WELCOME TO THE CALORIC INTAKE PROGRAM!");
-							
-				//Prompt the user for his/ her name:
-				stringInput = JOptionPane.showInputDialog("Please enter your name:          ");
-				
-				//Display gender question and prompt user for input
-				stringInput = JOptionPane.showInputDialog("Please select your gender\n"
-				+"\n1) Male\n"+"\n2) Female\n\n"+"Please select your answer:");
-				
-				//Display activity level question and prompt user for input
-				stringInput = JOptionPane.showInputDialog("Please select your activity level\n"
-				+"\n1) Moderate Activity\n"+"\n2) Low Level Activity\n\n"+"Please select your answer:");
-					
-				//Prompt the user for his/ her weight:
-				userWeight = JOptionPane.showInputDialog("Please enter your weight (lb):          ");				
-				
-				//validate weight
-				/*if (weight <=0);
-				JOptionPane.showMessageDialog(null, "INVALID INPUT!");
-					
-				}
-				
-				while (weight<=0);*/
-				
-				//convert the user's input to a double and we store in variable
-				//weight = Double.parseDouble(stringInput);
-				
-				//one-way if statement to display caloric intake
-				
-			
-				//display goodbye
-				JOptionPane.showMessageDialog(null, "Have a great day!!");
+    public static void main(String[] args) {
 
-	}
+        // Declare variables
+        String input; // needed to store input from a dialog box
+        int weight;
+        int caloricNeed = 0;
+        int gender;
+        int activityLevel;
 
+        // Display intro using a dialog box
+        JOptionPane.showMessageDialog(null, "Welcome To The Daily " 
+        		+ "Recommended Calorie Intake Program!\n");
+
+        // Prompt the user for his/ her weight:
+        input = JOptionPane.showInputDialog("Please enter your weight: ");
+        weight = Integer.parseInt(input);
+        //Validate weight 
+        if (weight < 10) {
+        	JOptionPane.showMessageDialog(null, "Invalid input, try again!\n");
+        } 
+        // Prompt the user for his/ her gender:
+        input = JOptionPane.showInputDialog("Please select the number that reflects your gender:\n\n"
+                +  "1) Male\n\n2) Female\n\n");
+        gender = Integer.parseInt(input);
+        //Validate weight 
+        if (gender != 1 &&gender != 2) {
+        	JOptionPane.showMessageDialog(null, "Invalid input, try again!\n");
+        }
+
+        // Prompt the user for his/ her activity level:
+        input = JOptionPane.showInputDialog("Please select the number that " 
+        		+ "reflects your activity level:\n\n"
+                +  "1) Moderately Active\n\n2) Low Activity level\n\n");
+        activityLevel = Integer.parseInt(input);
+        
+        //Decision structure to determine calories
+        if (gender == 1) {
+            if (activityLevel == 1) {
+                caloricNeed = weight * 15;
+            } else if (activityLevel == 2) {
+                caloricNeed = weight * 13;
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid input, try again!\n");
+            }
+        }
+        else if (gender == 2) {
+            if (activityLevel == 1) {
+                caloricNeed = weight * 12;
+            } else if (activityLevel == 2) {
+                caloricNeed = weight * 10;
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid input, try again!\n");
+            }
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Invalid input, try again!\n");
+        }
+
+        // Display recommended daily caloric intake using a dialog box
+        JOptionPane.showMessageDialog(null, "Your daily recommended calorie intake is: \n\n" 
+        + "                    " + caloricNeed + " calories\n\n");
+        
+        //Display goodbye
+      		JOptionPane.showMessageDialog(null, "       Have a great day!");
+
+        System.exit(0); // needed when using dialog boxes
+    }
 }
